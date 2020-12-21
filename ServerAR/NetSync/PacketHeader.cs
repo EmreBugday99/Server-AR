65a7b2d8ff0fe8b91aa2d91ddad4418ed4a3c4fe
Emre Bugday <47198270+EmreB99@users.noreply.github.com>
2020-12-21 04:54:29 +0300

    Imported NetSync
    I forgot to add NetSync at my initial commit. Added it now.

---
+++ ServerAR/NetSync/PacketHeader.cs
@@ -0,0 +1,14 @@
+﻿namespace NetSync
+{
+    public readonly struct PacketHeader
+    {
+        public readonly byte Channel;
+        public readonly byte PacketId;
+
+        public PacketHeader(byte channel, byte packetId)
+        {
+            Channel = channel;
+            PacketId = packetId;
+        }
+    }
+}
